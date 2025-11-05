1dcd8e1b5812f18a1074a5436231b7c2db1f0a23
MrKonoplyanka <ivanriloferreiro@gmail.com>
2025-11-05 23:09:03 +0100

    ..

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { toSignal, rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
+import { Card } from "@products/component/product-card/product-card";
+
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [Card],
   templateUrl: './gender-page.html',
 })
-export class GenderPage { }
+export class GenderPage {
+  route = inject(ActivatedRoute);
+  productsService = inject(ProductsService)
+
+  gender = toSignal(
+    this.route.params.pipe(
+      map(({gender}) => gender)
+    )
+  )
+productsResource = rxResource({
+    params: () => ({gender:this.gender()}),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({
+        gender: params.gender
+
+      });
+    }
+  })
+}
--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -5,18 +5,22 @@ import { ProductsService } from '@products/services/products.service';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { Observable, tap } from 'rxjs';
 import { Product, ProductsResponse } from '@products/interfaces/product.interface';
+import { Pagination } from '@shared/components/pagination/pagination';
+
+
 
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [Card],
+  imports: [Card, Pagination],
   templateUrl: './home-page.html',
 
 })
 export class HomePage {
   productsService = inject(ProductsService);
   products = signal<Product[]>([]);
+
 
 
   productsResource = rxResource({
